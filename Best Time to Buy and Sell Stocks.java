369ef30b7c5ae37933563ac410b8ce879e21468d
Sohan Kumar Rout <104723401+SohanRout2023@users.noreply.github.com>
2024-07-20 01:44:43 +0530

    Create Best Time to Buy and Sell Stocks.java

---
+++ Best Time to Buy and Sell Stocks.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxprofit=0;
+       int  minsofar=prices[0];
+        for(int i =0;i<prices.length;i++)
+        {
+            minsofar=Math.min(minsofar,prices[i]);
+            int profit=prices[i]-minsofar;
+            maxprofit=Math.max(maxprofit,profit);
+        }
+        return maxprofit;
+        
+    }
+}
